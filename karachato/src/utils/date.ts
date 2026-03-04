a7fc99d880479e632a54f70ae54cd04e3922ab39
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-04 14:42:35 +0900

    feat: 현재 날짜 추출 유틸 함수 생성

---
+++ karachato/src/utils/date.ts
@@ -0,0 +1,9 @@
+export function getToday() {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const date = String(now.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${date}`;
+}
