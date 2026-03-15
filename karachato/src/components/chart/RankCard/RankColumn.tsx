71bb3e2d98617128827aa9101574877af875a365
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-16 03:14:37 +0900

    feat: RankColumn 컴포넌트 추가

---
+++ karachato/src/components/chart/RankCard/RankColumn.tsx
@@ -0,0 +1,16 @@
+import RankBadge from "@/components/common/badges/RankBadge";
+import type { DeltaStatus } from "@/types/domain";
+
+export interface RankColumnProps {
+  rank: number;
+  status: DeltaStatus;
+}
+
+export default function RankColumn({ rank, status }: RankColumnProps) {
+  return (
+    <div className="w-10 h-15 gap-0.5 flex flex-col justify-center items-center">
+      <p className="typo-title-02 text-content-primary">{rank}</p>
+      <RankBadge status={status} />
+    </div>
+  );
+}
