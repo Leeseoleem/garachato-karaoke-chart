509bc22ca830247cb6f415aeabb6a2a34158536b
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-18 01:51:14 +0900

    feat: ScrollToTopButton, SettingsButton 컴포넌트 추가

---
+++ karachato/src/components/common/buttons/SettingsButton.tsx
@@ -0,0 +1,23 @@
+// SettingsButton.tsx
+import { Settings } from "lucide-react";
+import IconButton from "./IconButton";
+import type { Size } from "./IconButton";
+
+interface SettingsButtonProps {
+  size?: Size;
+  onClick: () => void;
+}
+
+export default function SettingsButton({
+  size = "large",
+  onClick,
+}: SettingsButtonProps) {
+  return (
+    <IconButton
+      size={size}
+      icon={<Settings size={20} color="#ffffff" />}
+      onClick={onClick}
+      ariaLabel="설정"
+    />
+  );
+}
