17de6147f55909bd075b784c22fce58c52ea8c24
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-17 16:48:04 +0900

    feat: ChartContainer 레이아웃 및 스크롤 영역 설정

--- karachato/src/components/chart/ChartContainer/index.tsx
+++ karachato/src/components/chart/ChartContainer/index.tsx
@@ -8,7 +8,7 @@ import { MOCK_CHART_ITEMS } from "@/lib/mock";
 
 export default function ChartContainer() {
   return (
-    <div>
+    <div className="flex h-full flex-1 flex-col bg-linear-to-b from-brand-dark to-gray-30 overflow-hidden">
       <KaraokeTabs />
       <ChartHeader />
       <ChartScrollContainer items={MOCK_CHART_ITEMS} />
