e464d0e13d61c27d60ee7d9247712dbf46d17fea
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-18 17:44:17 +0900

    feat: SearchHeader 컴포넌트 및 스토리 추가

---
+++ karachato/src/components/common/headers/SearchHeader.tsx
@@ -0,0 +1,37 @@
+// === component ===
+import Header from "./Header";
+import BackButton from "./BackButton";
+import SearchInput, {
+  type SearchInputProps,
+} from "@/components/search/SearchInput";
+import ChatButton from "../buttons/ChatButton";
+
+type Mode = "default" | "search";
+
+interface SearchHeaderProps {
+  mode?: Mode;
+  search: SearchInputProps;
+}
+
+export default function SearchHeader({
+  mode = "default",
+  search,
+}: SearchHeaderProps) {
+  return (
+    <Header className="gap-3 py-6">
+      {mode === "search" && (
+        <div className="shrink-0">
+          <BackButton />
+        </div>
+      )}
+      <div className="flex-1">
+        <SearchInput {...search} />
+      </div>
+      {mode === "default" && (
+        <div className="shrink-0">
+          <ChatButton />
+        </div>
+      )}
+    </Header>
+  );
+}
