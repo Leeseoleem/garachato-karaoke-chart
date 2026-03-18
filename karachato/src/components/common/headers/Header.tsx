a5bd665f650be9fc82f57499f87578e189922020
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-18 17:42:05 +0900

    feat: Header 컴포넌트 생성

---
+++ karachato/src/components/common/headers/Header.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Header({ children, className }: HeaderProps) {
+  return (
+    <div
+      className={clsx(
+        "flex flex-row w-full min-h-16 px-4 items-center",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
