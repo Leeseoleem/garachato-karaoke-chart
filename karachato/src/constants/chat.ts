ec2bd95e662e0f0f86acd3944cb6b75cf608f6c0
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-23 18:16:14 +0900

    feat: 채팅 관련 상수 파일 추가

---
+++ karachato/src/constants/chat.ts
@@ -0,0 +1,13 @@
+export const ARTIST_KO_MAP = {
+  YOASOBI: "요아소비",
+  米津玄師: "요네즈켄시",
+  Ado: "아도",
+  あいみょん: "아이묭",
+  Official髭男dism: "히게단",
+  "Mrs. Green Apple": "Mrs. Green Apple",
+} as const;
+
+export const STATIC_QUICK_QUESTIONS = [
+  "요즘 가장 인기 있는 노래는?",
+  "최근 SNS에서 유행 중인 노래 찾아줘",
+] as const;
