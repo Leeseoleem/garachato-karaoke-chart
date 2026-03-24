4bdb796253ef7ce60ef394629498ec37570cbdae
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-25 04:38:10 +0900

    feat: SearchPageSkeleton 컴포넌트 및 스토리 추가

---
+++ karachato/src/components/skeletons/pages/SearchPageSkeleton.tsx
@@ -0,0 +1,19 @@
+import { ChevronLeft } from "lucide-react";
+import { SearchResultItemSkeleton } from "../SearchResultSkeleton";
+
+export function SearchPageSkeleton() {
+  return (
+    <div className="flex h-dvh flex-col">
+      <div className="flex flex-row w-full h-25 px-4 items-center gap-3">
+        <ChevronLeft size={24} color="#B294EE" />
+        <div className="w-full px-5 pr-12 h-13 rounded-full search-border outline-none bg-transparent" />
+      </div>
+      <div className="flex w-full justify-end items-center h-11">
+        <p className="typo-description text-content-secondary">0개의 결과</p>
+      </div>
+      {Array.from({ length: 3 }).map((_, i) => (
+        <SearchResultItemSkeleton key={i} />
+      ))}
+    </div>
+  );
+}
