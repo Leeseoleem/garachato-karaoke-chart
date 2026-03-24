b39541c378287e83bdb4921dc36f182a9fbbb701
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-25 04:29:16 +0900

    feat: 검색창 영역 추가

--- karachato/src/components/skeletons/pages/MainPageSkeleton.tsx
+++ karachato/src/components/skeletons/pages/MainPageSkeleton.tsx
@@ -3,7 +3,10 @@ import { ChartListSkeleton } from "../ChartListSkeleton";
 export function MainPageSkeleton() {
   return (
     <div className="flex h-dvh flex-col">
-      <div className="h-16" />
+      <div className="flex flex-row w-full h-25 px-4 items-center gap-3">
+        <div className="w-full px-5 pr-12 h-13 rounded-full search-border outline-none bg-transparent" />
+        <div className="shrink-0 flex items-center justify-center rounded-full glass h-13 w-13" />
+      </div>
       <ChartListSkeleton />
     </div>
   );
