ec38df0eef2a3f8756402fcab130a00c7602aad4
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-03-30 13:11:53 +0900

    chore: 임시 처리용

--- karachato/src/utils/auth.ts
+++ karachato/src/utils/auth.ts
@@ -1,10 +1,5 @@
 export function checkAuth(request: Request): boolean {
-  if (!process.env.CRON_SECRET) {
-    if (process.env.NODE_ENV === "production") {
-      return false;
-    }
-    return true;
-  }
+  if (!process.env.CRON_SECRET) return true;
   return (
     request.headers.get("authorization") === `Bearer ${process.env.CRON_SECRET}`
   );
