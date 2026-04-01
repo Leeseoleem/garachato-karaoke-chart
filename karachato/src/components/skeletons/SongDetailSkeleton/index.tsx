c5eb9d6b6dd8cfd76a6779cd7c7c53983efc6bac
Lee seo Leem <138491614+Leeseoleem@users.noreply.github.com>
2026-04-01 17:25:37 +0900

    fix: 스텔레톤 내부 실제 페이지와 유사하게 조정

--- karachato/src/components/skeletons/SongDetailSkeleton/index.tsx
+++ karachato/src/components/skeletons/SongDetailSkeleton/index.tsx
@@ -1,3 +1,4 @@
+import BackHeader from "@/components/common/headers/BackHeader";
 import { HeroSkeleton } from "./HeroSkeleton";
 import { InfoSkeleton } from "./InfoSkeleton";
 import { VocalGuideSkeleton } from "./VocalGuideSkeleton";
@@ -5,9 +6,12 @@ import { VocalGuideSkeleton } from "./VocalGuideSkeleton";
 export function SongDetailSkeleton() {
   return (
     <div className="flex h-dvh flex-col">
-      <HeroSkeleton />
-      <InfoSkeleton />
-      <VocalGuideSkeleton />
+      <BackHeader title="곡 상세 정보" />
+      <div className="flex-1 overflow-y-auto p-6">
+        <HeroSkeleton />
+        <InfoSkeleton />
+        <VocalGuideSkeleton />
+      </div>
     </div>
   );
 }
